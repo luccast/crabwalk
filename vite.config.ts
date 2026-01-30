f013badc14f03aa4786714b9433b0685eb3cc1e1
nktfh100 <me@malachi.io>
2026-01-30 12:45:18 +0200

    Remove ALLOWED_HOSTS

--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,7 @@ import viteReact from '@vitejs/plugin-react';
 import viteTsConfigPaths from 'vite-tsconfig-paths';
 import tailwindcss from '@tailwindcss/vite';
 
-const allowedHosts = process.env.ALLOWED_HOSTS
-	? process.env.ALLOWED_HOSTS.split(',')
-			.map((host) => host.trim())
-			.filter(Boolean)
-	: undefined;
-
 export default defineConfig({
-	server: {
-		allowedHosts,
-	},
 	plugins: [
 		viteTsConfigPaths({
 			projects: ['./tsconfig.json'],
